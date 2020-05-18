ff2af314f178280c1e0c7bcf0f1080e3c8346ad4
Rainy <rainy4499@gmail.com>
2020-05-18 15:50:21 +0800

    add fliter

--- RazorPracite/Models/Movie.cs
+++ RazorPracite/Models/Movie.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPracite.Models
-{
+{ 
     public class Movie
     {
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [DisplayName("Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
--- RazorPracite/Pages/Movies/Index.cshtml.cs
+++ RazorPracite/Pages/Movies/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPracite.Data;
 using RazorPracite.Models;
@@ -17,10 +20,31 @@ namespace RazorPracite.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var genreQuery = _context.Movie.OrderBy(r => r.Genre).Select(r => r.Genre);
+
+            var movies = _context.Movie.Select(r => r);
+                
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
